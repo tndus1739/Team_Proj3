db5f0fced5d2aa0134d4ceff92ef80edafe1ae6d
sooyeon noh <tndus1739@gmial.com>
2024-02-13 00:12:34 -0800

    처음

---
+++ src/test/java/com/example/demo/TeamProj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProj3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
